fa7ac059c7a7ca2a6f00f6619039e6af5036c233
Hewon Jeong <hewon.jng@gmail.com>
2019-10-08 19:05:56 +0900

    Initial commit

---
+++ src/__stories__/useEffectOnce.story.tsx
@@ -0,0 +1,19 @@
+import { storiesOf } from '@storybook/react';
+import * as React from 'react';
+import { useEffectOnce } from '..';
+import ConsoleStory from './util/ConsoleStory';
+
+const Demo = () => {
+  useEffectOnce(() => {
+    console.log('Running effect once on mount');
+
+    return () => {
+      console.log('Running clean-up of effect on unmount');
+    };
+  });
+
+  return <ConsoleStory />;
+};
+
+storiesOf('Lifecycle|useEffectOnce', module)
+  .add('Demo', () => <Demo />);
---
+++ src/__stories__/useMount.story.tsx
@@ -0,0 +1,13 @@
+import { storiesOf } from '@storybook/react';
+import * as React from 'react';
+import { useMount } from '..';
+import ConsoleStory from './util/ConsoleStory';
+
+const Demo = () => {
+  useMount(() => console.log('MOUNTED'));
+
+  return <ConsoleStory />;
+};
+
+storiesOf('Lifecycle|useMount', module)
+  .add('Demo', () => <Demo />);
